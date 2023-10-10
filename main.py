666cb9614964f3b3d8b55f846385e1cda6dd2392
İlker Civil <ilkercivil@gmail.com>
2023-10-10 14:22:35 +0300

    turtle kare çizimi for loop ile

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ drawing_board= turtle.Screen()
 drawing_board.bgcolor("green")
 drawing_board.title("Python Turtle")
 turtle_instance= turtle.Turtle()
-
+'''''
 turtle_instance.forward(100)
 turtle_instance.left(90)
 turtle_instance.forward(100)
@@ -11,6 +11,11 @@ turtle_instance.left(90)
 turtle_instance.forward(100)
 turtle_instance.left(90)
 turtle_instance.forward(100)
-
 
 turtle.done()
+'''
+for i in range(4):
+    turtle_instance.forward(100)
+    turtle_instance.left(90)
+turtle.done()
+
