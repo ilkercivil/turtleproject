3a590bef47115039b7e5831f6ab8027cfa4ba3e4
İlker Civil <ilkercivil@gmail.com>
2023-10-11 09:16:51 +0300

    yıldız eklendi

--- main.py
+++ main.py
@@ -14,8 +14,19 @@ turtle_instance.forward(100)
 
 turtle.done()
 '''
+'''
 for i in range(4):
     turtle_instance.forward(100)
     turtle_instance.left(90)
 turtle.done()
+'''
+star_turtle=turtle.Turtle()
+side_length=100
+interior_angle = 180 - (360 / 5)
+for i in range(5):
+    star_turtle.forward(side_length)
+    star_turtle.right(interior_angle)
+    star_turtle.forward(side_length)
+    star_turtle.left(72 - interior_angle)
 
+turtle.done()
